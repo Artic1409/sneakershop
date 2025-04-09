cab1cc25aec4a7ca32772d59dbcb28ecd62ed573
Artic1409 <artic1409@yandex.ru>
2025-04-10 00:40:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ const supabaseUrl = 'https://tqeeemvewrvlnsdhlkal.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxZWVlbXZld3J2bG5zZGhsa2FsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyMzE5ODAsImV4cCI6MjA1OTgwNzk4MH0.iu8q0lq7m0wr9ii9lNT3ODu2lOuMOdbsU4A2xPfdWUI';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
-const supabase = window.supabase.createClient(
-  SUPABASE_CONFIG.url, 
-  SUPABASE_CONFIG.key
-);
+const supabase = window.supabase.createClient(SUPABASE_CONFIG.url, SUPABASE_CONFIG.key);
 
 // DOM элементы
 const elements = {
